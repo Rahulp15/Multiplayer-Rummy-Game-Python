6d010edb7d53bbc522de15d7a5f56bd9b02412d3
Rahulp15 <rahulpatil15102000@gmail.com>
2022-02-12 19:49:05 +0530

    init

--- shuffle.py
+++ shuffle.py
@@ -2,6 +2,7 @@ import random
 SUIT = ["H","C","S","D"]
 RANK = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"]
 stock_deck = [(r + s) for s in SUIT for r in RANK]
+stock_deck.extend(stock_deck)
 random.shuffle(stock_deck)
 print("Playing 10 Cards")
 for i in range(10):
